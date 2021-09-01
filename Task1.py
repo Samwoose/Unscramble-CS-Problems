d11f7a9284cc9fca4408893635236c82bce16fd7
Samwoo Seong <tjdtkadn@gmail.com>
2021-09-01 12:18:47 +0900

    Complete Task1

--- Task1.py
+++ Task1.py
@@ -18,3 +18,21 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+count = 0
+phone_num_list = []
+
+def phone_num_adder(phone_num_list, all_records):
+    num_records = len(all_records)
+    #print('current number of records is',num_records)
+    for i in range(num_records):
+        if all_records[i][0] not in phone_num_list:
+            phone_num_list.append(all_records[i][0])
+    
+    return phone_num_list
+
+phone_num_list1 = phone_num_adder(phone_num_list, texts)
+phone_num_list2 = phone_num_adder(phone_num_list1, calls)
+
+count = len(phone_num_list2)
+
+print("There are", count, "different telephone numbers in the records.")
